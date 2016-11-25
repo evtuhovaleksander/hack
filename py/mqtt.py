209391b8033e34dc5dc4ae854554217d737e53aa
evtuhovaleksander <evtuhov.aleksander@gmail.com>
2016-11-25 13:02:24 +0300

    com

--- py/mqtt.py
+++ py/mqtt.py
@@ -1,14 +1,10 @@
-
-GROUP_ID = 1
 import json
 
-#
-# MQTT client.
-#
 import ibmiotf.device
-import RPIO
+from uti import *
 
-PUBLISH_TOPIC=''
+PUBLISH_TOPIC = 'iot-2/evt/<event-id>/fmt/<format>'
+
 
 def connect(config):
     options = ibmiotf.device.ParseConfigFile(config)
@@ -18,41 +14,35 @@ def connect(config):
     return client
 
 
-
 def on_message(cmd):
-    if cmd.command != 'button':
-        return
-    inpstr=cmd.command;
+
+    inpstr = cmd.command;
     parsed_string = json.loads(inpstr)
-    fullID=parsed_string["lotID"]
+    fullID = parsed_string["lotID"]
+    a = fullID[0];
+    b = fullID[2];
+    ind = int(a) * 4 + int(b)
+    update_status(ind, 1)
 
 
+def send_data(ident, status):
 
-
-
-def send_data(ident,status):
-    #topic = PUBLISH_TOPIC
-    #a=ident/4
-    #b=ident%4
-    #jsonID=a+'_'+b
-    if(status==1):
-        data='blocked'
+    if (status == 1):
+        st = 'blocked'
     else:
-        data='free'
-    data='{"lotID": '+ident+',"status": "'+st+'"}'
-    client.publishEvent(topic, 'json', data)
+        st = 'free'
+    data = '{"lotID": ' + ident + ',"status": "' + st + '"}'
+    client.publishEvent(PUBLISH_TOPIC, 'json', data)
+
 
 client = connect('device.cfg')
 
+
 def main():
     while True:
-        payload = receive_if_available()
-        if payload:
-            sid, data = payload
-            if 0 <= sid <= 1:
-                send_data(sid, data)
-        else:
-            time.sleep(0.1)
+        for i in range(1, 2):
+            send_data(i, get_status(i))
+
 
 if __name__ == '__main__':
     main()
